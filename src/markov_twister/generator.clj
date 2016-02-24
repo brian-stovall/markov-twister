fe17ba073b25533ebe42491cacddffb754a89515
Brian Stovall <hoppenmaus@gmail.com>
2016-02-24 11:56:04 -0500

    file->story complete. refactored word-chain->story to take a maximal word count and terminate when it is reached

--- src/markov_twister/generator.clj
+++ src/markov_twister/generator.clj
@@ -10,15 +10,23 @@
 (defn text->word-chain [text]
   (word-chain (partition-all 3 1 (clojure.string/split text #"[\s|\n]"))))
 
-(defn walk-chain [prefix chain accumulator]
+(defn word-count [text]
+  (count (clojure.string/split text #"[\s|\n]")))
+
+(defn walk-chain [prefix chain accumulator length]
   (let [suffixes (get chain prefix)]
-    (if (empty? suffixes)
+    (if (or (> (count accumulator) length) (empty? suffixes))
       accumulator
       (let [suffix (first (shuffle suffixes))
             new-prefix [(last prefix) suffix]]
-        (recur new-prefix chain (conj accumulator suffix))))))
+        (recur new-prefix chain (conj accumulator suffix) length)))))
 
-(defn text->story [text]
-  (let [chain (text->word-chain text)
-        random-prefix (first (shuffle (keys chain)))]
-    (clojure.string/join " " (walk-chain random-prefix chain []))))
+(defn word-chain->story [chain length]
+  (let [random-prefix (first (shuffle (keys chain)))]
+    (clojure.string/join " " (walk-chain random-prefix chain [] length))))
+
+(defn file->word-chain [filename]
+  (text->word-chain (slurp (clojure.java.io/resource filename))))
+
+(defn file->story [filename length]
+  (word-chain->story (file->word-chain filename) length))
