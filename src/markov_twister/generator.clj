bcdb3565f904eed7c32d5c0a8396dbfd7ccc7209
Brian Stovall <hoppenmaus@gmail.com>
2016-02-24 09:15:45 -0500

    text->story fn complete

--- src/markov_twister/generator.clj
+++ src/markov_twister/generator.clj
@@ -9,3 +9,16 @@
 
 (defn text->word-chain [text]
   (word-chain (partition-all 3 1 (clojure.string/split text #"[\s|\n]"))))
+
+(defn walk-chain [prefix chain accumulator]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      accumulator
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]]
+        (recur new-prefix chain (conj accumulator suffix))))))
+
+(defn text->story [text]
+  (let [chain (text->word-chain text)
+        random-prefix (first (shuffle (keys chain)))]
+    (clojure.string/join " " (walk-chain random-prefix chain []))))
