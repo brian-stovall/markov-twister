ee201617d463a13200f374e5ce168546684ba9cf
Brian Stovall <hoppenmaus@gmail.com>
2016-02-24 20:33:53 -0500

    added fns to make word-chain from multiple inputs. added code to sanitize output so that it begins with a capitalized word and ends with proper punctuation

--- src/markov_twister/generator.clj
+++ src/markov_twister/generator.clj
@@ -8,25 +8,35 @@
           word-transitions))
 
 (defn text->word-chain [text]
-  (word-chain (partition-all 3 1 (clojure.string/split text #"[\s|\n]"))))
+  (word-chain (partition-all 3 1 (clojure.string/split text #"\s+"))))
 
-(defn word-count [text]
-  (count (clojure.string/split text #"[\s|\n]")))
+(defn ending-punctuation? [char]
+  (some #{\. \! \?} char))
+
+(defn capitalized? [string]
+  (re-matches #"^[A-Z].+" string))
 
 (defn walk-chain [prefix chain accumulator length]
   (let [suffixes (get chain prefix)]
-    (if (or (> (count accumulator) length) (empty? suffixes))
+    (if (or (and  (> (count accumulator) length) (ending-punctuation? (last accumulator)))
+            (empty? suffixes))
       accumulator
       (let [suffix (first (shuffle suffixes))
             new-prefix [(last prefix) suffix]]
         (recur new-prefix chain (conj accumulator suffix) length)))))
 
 (defn word-chain->story [chain length]
-  (let [random-prefix (first (shuffle (keys chain)))]
-    (clojure.string/join " " (walk-chain random-prefix chain [] length))))
+  (let [random-prefix (first (shuffle (keys chain)))
+        word-list (walk-chain random-prefix chain [] length)
+        sanitized-words (drop-while (complement capitalized?) word-list)]   
+     (clojure.string/join " " sanitized-words)))
 
 (defn file->word-chain [filename]
   (text->word-chain (slurp (clojure.java.io/resource filename))))
 
 (defn file->story [filename length]
   (word-chain->story (file->word-chain filename) length))
+
+(defn files->story [length & files]
+  (let [chains (map file->word-chain files)]
+    (word-chain->story (apply merge-with clojure.set/union chains) length)))
